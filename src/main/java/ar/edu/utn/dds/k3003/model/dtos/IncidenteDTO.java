b5a3fdaa0bfdc105ace42c773d92f6f31e2d63aa
santinocarrizoo04 <santino.carrizo25@gmail.com>
2024-11-28 19:32:10 -0300

    Incidente generated value

--- src/main/java/ar/edu/utn/dds/k3003/model/dtos/IncidenteDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/dtos/IncidenteDTO.java
@@ -6,10 +6,7 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,7 +15,7 @@ import java.time.LocalDateTime;
 @Table(name = "Incidentes")
 public class IncidenteDTO {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "tipo")
     private TipoIncidenteEnum tipoIncidente;
